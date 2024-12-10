f53fa862f794f7093436e37d3e0c238aaacd1f38
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-12-10 13:56:45 +0000

    two layers of squares added

--- sketch2.js
+++ sketch2.js
@@ -1,19 +1,30 @@
+const COLOURS = ["#FF0000", "#FFFF00", "#FFFFF0", "#00008B", "#FFFFF0", "#000000"];
+
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(780, 600);
     noLoop();
-    
 }
 
 function draw() {
-    background(220);
-    
+    background(255);
+    noStroke();
+
     const CELLSIZE = 100;
-    for(let y = 0; y < height; y += CELLSIZE) {
-      for(let x = 0; x < width; x += CELLSIZE) {
+    for(let y = 10; y < height; y += CELLSIZE) {
+      for(let x = 10; x < width; x += CELLSIZE) {
         fill(random(255));
-        let x = width/2; 
-        let y = height/2;
-        circle(x, y, CELLSIZE);
+        rect(x, y, CELLSIZE);
+        x += CELLSIZE;
       }
+      y += CELLSIZE;
     }
+
+    for(let y = 10; y < height; y += CELLSIZE) {
+        for(let x = 50; x < width; x += CELLSIZE) {
+          fill(random(255));
+          rect(x, y, CELLSIZE);
+          x += CELLSIZE;
+        }
+        y += CELLSIZE;
+      }
 }
