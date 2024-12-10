bd0ac454ebd093ea091d73a97c16066d081d39e6
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-12-10 13:44:39 +0000

    beginning of circles

--- sketch2.js
+++ sketch2.js
@@ -1,8 +1,19 @@
 function setup() {
     createCanvas(800, 800);
+    noLoop();
+    
 }
 
 function draw() {
     background(220);
     
+    const CELLSIZE = 100;
+    for(let y = 0; y < height; y += CELLSIZE) {
+      for(let x = 0; x < width; x += CELLSIZE) {
+        fill(random(255));
+        let x = width/2; 
+        let y = height/2;
+        circle(x, y, CELLSIZE);
+      }
+    }
 }
