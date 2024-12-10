c2a6802d3e96e02ed72132b077ffa3a1b9b160ba
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-12-10 14:47:38 +0000

    finish idea 1 begin on idea 2

--- sketch2.js
+++ sketch2.js
@@ -1,4 +1,5 @@
-const COLOURS = [];
+const COLOURS = ["#2768B790", "#8A64D690", "#9AD66490", "#D6649190"];
+const SOLIDCOLOURS = ["#EEE64B", "#4DEF7F", "#AB4DEF", "#EFAD4D", "#BFD641"]
 
 function setup() {
     createCanvas(800, 600);
@@ -6,25 +7,36 @@ function setup() {
 }
 
 function draw() {
-    background(255);
+    background(255, 10);
     noStroke();
 
+    /////////////
+    //IDEA 1/////
+    /////////////
+    /*
+    //First layer
     const CELLSIZE = 100;
-    for (let y = 50; y < height; y += CELLSIZE) {
+    for (let y = 100; y < height; y += CELLSIZE) {
         for (let x = 100; x < width; x += CELLSIZE) {
-            fill(238, 230, 75);
-            circle(x, y, CELLSIZE);
+            fill(random(SOLIDCOLOURS));
+            circle(x, y, CELLSIZE); // x + random(-10, 10), y + random(-10, 10), CELLSIZE
             x += CELLSIZE;
         }
         y += CELLSIZE;
     }
 
-    for (let y = 50; y < height; y += CELLSIZE) {
+    //Second layer
+    for (let y = 100; y < height; y += CELLSIZE) {
         for (let x = 140; x < width; x += CELLSIZE) {
-            fill(39, 87, 183, 120);
-            circle(x, y, CELLSIZE);
+            fill(random(COLOURS));
+            circle(x, y, CELLSIZE); // x + random(-10, 10), y + random(-10, 10), CELLSIZE
             x += CELLSIZE;
         }
         y += CELLSIZE;
-    }
+    } */
+
+    /////////////
+    //IDEA 2/////
+    /////////////
+    const CELLSIZE = 80;
 }
