856c5e433db8a94f0e551db35ee3555f805a7267
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-12-12 12:00:45 +0000

    adding mouse pressing

--- sketch3.js
+++ sketch3.js
@@ -1,3 +1,4 @@
+let isPressing = false;
 let gridCoulours = [];
 const COLOURS = ["#2768B790", "#8A64D690", "#9AD66490", "#D6649190"];
 const CELLSIZE = 80;
@@ -30,23 +31,25 @@ function draw() {
     for (let y = 0; y < height; y += CELLSIZE) {
         for (let x = 0; x < width; x += CELLSIZE) {
 
-            fill(gridCoulours[colourIndex]);
-            arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE, CELLSIZE,  angle + 45, angle - 45);
-    
-            fill(gridCoulours[colourIndex]);
-            arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 1.3, CELLSIZE / 1.3, angle1 + 90, angle1 - 90);
-            
-            fill(gridCoulours[colourIndex]);
-            arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 2, CELLSIZE / 2, angle2 + 135, angle2 - 135);
-  
-            fill(gridCoulours[colourIndex]);
-            arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 3, CELLSIZE / 3, angle3 + 90, angle3 - 90);
-
-            fill(gridCoulours[colourIndex]);
-            arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 4, CELLSIZE / 4, angle4 + 135, angle4 - 135);
-            colourIndex++;
+            if(isPressing && mouseX >= x && mouseX <= x + cellSize && mouseY >= y && mouseY <= y + cellSize){
+                fill("#13229145")
+                arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE, CELLSIZE,  angle + 45, angle - 45);
+                arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 1.3, CELLSIZE / 1.3, angle1 + 90, angle1 - 90);            
+                arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 2, CELLSIZE / 2, angle2 + 135, angle2 - 135);  
+                arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 3, CELLSIZE / 3, angle3 + 90, angle3 - 90);
+                arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 4, CELLSIZE / 4, angle4 + 135, angle4 - 135);
+            } else {
+                fill(gridCoulours[colourIndex]);
+                arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE, CELLSIZE,  angle + 45, angle - 45);        
+                arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 1.3, CELLSIZE / 1.3, angle1 + 90, angle1 - 90);                
+                arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 2, CELLSIZE / 2, angle2 + 135, angle2 - 135);     
+                arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 3, CELLSIZE / 3, angle3 + 90, angle3 - 90);   
+                arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 4, CELLSIZE / 4, angle4 + 135, angle4 - 135);
+                colourIndex++;
+            }
         }
     } 
+
     angle++;
     angle1++;
     angle2++;
