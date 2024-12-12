2f732adec2a25731254b88f42a143f4e3e7f9ec0
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-12-12 10:56:51 +0000

    add rotation to circles

--- sketch3.js
+++ sketch3.js
@@ -1,17 +1,42 @@
+let gridCoulours = [];
+const COLOURS = ["#2768B790", "#8A64D690", "#9AD66490", "#D6649190"];
+let angle = 0;
+const CELLSIZE = 80;
 
 function setup() {
     createCanvas(800, 800);
     //noLoop();
+    angleMode(DEGREES);
+
+    for (let y = 0; y < height; y += CELLSIZE) {
+        for (let x = 0; x < width; x += CELLSIZE) {
+            gridCoulours.push(random(COLOURS))
+        }
+    }
 }
 
 function draw() {
     background(255);
-    // let angle = frameCount * 0.01;
-    // rotate(angle);
-    // let axis = createVector(1, 1, 0);
-    // rotate(QUARTER_PI, axis);
-    arc(50, 50, 80, 80, 0, PI + QUARTER_PI, PIE);
-    fill(0);
-    rotate(QUARTER_PI);
-    arc(100, 100, 80, 80, 0, PI + HALF_PI);
+
+    //colour index value
+    let colourIndex = 0;
+
+    for (let y = 0; y < height; y += CELLSIZE) {
+        for (let x = 0; x < width; x += CELLSIZE) {
+            fill(gridCoulours[colourIndex]);
+            arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE, CELLSIZE,  angle + 45, angle - 45);
+            // fill(gridCoulours[colourIndex + 1]);
+            // arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 1.3, CELLSIZE / 1.3, angle + 90, angle - 90);
+            // fill(gridCoulours[colourIndex]);
+            // arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 2, CELLSIZE / 2, angle + 45, angle - 45);
+            // fill(gridCoulours[colourIndex + 2]);
+            // arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 3, CELLSIZE / 3, angle + 90, angle - 90);
+            // fill(gridCoulours[colourIndex + 3]);
+            // arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 4, CELLSIZE / 4, angle + 135, angle - 135);
+            //angle ++;
+            colourIndex++;
+        }
+    } 
+    angle ++;
+
 }
